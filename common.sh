3fc65c2f8378137dd1019991b8c5e837d1938d13
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-24 14:15:01 +0530

    functions in roboshop

--- common.sh
+++ common.sh
@@ -1,44 +1,44 @@
 fun_nodejs() {
 
   log=/tmp/roboshop.log
-  echo -e "\e[35m<<<<<<<<<<  create {component} service  >>>>>>>>>\e[0m"
-  cp {component}.service /etc/systemd/system/{component}.service &>>{log}
+  echo -e "\e[35m<<<<<<<<<<  create $${component}} service  >>>>>>>>>\e[0m"
+  cp $${component}}.service /etc/systemd/system/$${component}}.service &>>$${log}
 
   echo -e "\e[35m<<<<<<<<<<  create mongodb repository  >>>>>>>>>\e[0m"
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>{log}
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  install nodejs repos  >>>>>>>>>\e[0m"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log}
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  install nodejs  >>>>>>>>>\e[0m"
-  yum install nodejs -y &>>{log}
+  yum install nodejs -y &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  create Application User >>>>>>>>>\e[0m"
-  useradd roboshop &>>{log}
+  useradd roboshop &>>${log}
 
   echo -e "\e[35m<<<<<<<<<< Create application directiory >>>>>>>>>\e[0m"
-  mkdir /app &>>{log}
+  mkdir /app &>>${log}
   echo -e "\e[35m<<<<<<<<<<  download Application Content  >>>>>>>>>\e[0m"
-  curl -o /tmp/{component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip &>>{log}
+  curl -o /tmp/${component}}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}}.zip &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  extract application content  >>>>>>>>>\e[0m"
-  cd /app &>>{log}
-  unzip /tmp/{component}.zip &>>{log}
-  cd /app &>>{log}
+  cd /app &>>${log}
+  unzip /tmp/${component}}.zip &>>${log}
+  cd /app &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  download nodejs dependencies  >>>>>>>>>\e[0m"
-  npm install &>>{log}
+  npm install &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  install mongodb client  >>>>>>>>>\e[0m"
-  yum install mongodb-org-shell -y &>>{log}
+  yum install mongodb-org-shell -y &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  load user schema  >>>>>>>>>\e[0m"
-  mongo --host mongodbtirupathib74.online </app/schema/catalogue.js &>>{log}
+  mongo --host mongodbtirupathib74.online </app/schema/catalogue.js &>>${log}
 
   echo -e "\e[35m<<<<<<<<<<  start user service  >>>>>>>>>\e[0m"
-  systemctl daemon-reload &>>{log}
-  systemctl enable {component} &>>{log}
-  systemctl restart {component} &>>{log}
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component}} &>>${log}
+  systemctl restart ${component}} &>>${log}
 
 
 }
