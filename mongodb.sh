1aa2eddedcaafbe1d768910157d5af096e50bdfe
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 09:21:07 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,14 @@
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  copy mongodb repo  >>>>>>>>>\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 func_exit_status
 
 echo -e "\e[35m<<<<<<<<<<  install mongodb >>>>>>>>>\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log}
 func_exit_status
 #update listen address from 127.0.0.1 to 0.0.0.
 
 echo -e "\e[35m<<<<<<<<<<  start mongodb service  >>>>>>>>>\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>${log}
+systemctl restart mongod &>>${log}
 func_exit_status
