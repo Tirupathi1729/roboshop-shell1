57890c1c0bcd5069bddeb5f9fb7027510ce23e7a
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 10:11:35 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,23 @@
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  download mysql repos  >>>>>>>>>\e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>${log}
 func_exit_status
 
 echo -e "\e[35m<<<<<<<<<<  disable mysql  >>>>>>>>>\e[0m"
-yum module disable mysql -y
+yum module disable mysql -y &>>${log}
 func_exit_status
 
 echo -e "\e[35m<<<<<<<<<<  install mysql  >>>>>>>>>\e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>${log}
 func_exit_status
 
 echo -e "\e[35m<<<<<<<<<<  start mysql  >>>>>>>>>\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>${log}
+systemctl start mysqld &>>${log}
 func_exit_status
 
 
 echo -e "\e[35m<<<<<<<<<<  give password  >>>>>>>>>\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+mysql -uroot -pRoboShop@1 &>>${log}
 func_exit_status
