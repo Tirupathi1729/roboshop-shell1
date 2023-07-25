f0c1fb5a231b1b680a7bf92a31b61e86c7d0c720
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 08:52:07 +0530

    exit status

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
 log=/tmp/roboshop.log
 func_exit_status() {
   if [ $? -eq 0 ];then
-    echo -e"\e[32m success\e[0m"
+    echo -e "\e[32m success\e[0m"
   else
-    echo -e"\e[32m failure\e[0m"
+    echo -e "\e[32m failure\e[0m"
   fi
 }
 func_apppreq() {
