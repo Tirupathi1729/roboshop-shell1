6ec7e7c76ef5e756f51ecea1ae6585f175c67b9e
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 10:02:17 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -6,8 +6,8 @@ func_exit_status
 echo -e "\e[35m<<<<<<<<<<  install mongodb >>>>>>>>>\e[0m"
 yum install mongodb-org -y &>>${log}
 func_exit_status
-echo -e "\e[35m<<<<<<<<<<Update listen address from 127.0.0.1 to 0.0.0.0>>>>>>>>>\e[0m"
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+echo -e "\e[35m<<<<<<<<<<   #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf >>>>>>>>>\e[0m"
+
 
 echo -e "\e[35m<<<<<<<<<<  start mongodb service  >>>>>>>>>\e[0m"
 systemctl enable mongod &>>${log}
