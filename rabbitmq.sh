eb7fa16bb41a9e78f4fa0530d062afe2dc9de633
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 09:43:41 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,29 +1,29 @@
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  install nginx  >>>>>>>>>\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log}
 func_exit_status
 
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  install nginx  >>>>>>>>>\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log}
 func_exit_status
 
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  install nginx  >>>>>>>>>\e[0m"
-yum install rabbitmq-server -y
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>${log}
+yum install rabbitmq-server -y &>>${log}
 func_exit_status
 
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  install nginx  >>>>>>>>>\e[0m"
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>>${log}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log}
 func_exit_status
 
 
 source common.sh
 echo -e "\e[35m<<<<<<<<<<  install nginx  >>>>>>>>>\e[0m"
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+systemctl enable rabbitmq-server &>>${log}
+systemctl restart rabbitmq-server &>>${log}
 func_exit_status
 
