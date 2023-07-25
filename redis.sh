7de55d2dc54617a552a42824af84f21e4f89b2b5
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 09:59:07 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,15 +1,20 @@
-echo -e "\e[35m<<<<<<<<<<redis fetching repo successfully>>>>>>>>>\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+source common.sh
+echo -e "\e[35m<<<<<<<<<<  install redis repo  >>>>>>>>>\e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
+func_exit_status
 
-echo -e "\e[35m<<<<<<<<<enabling redis 6.2 successfully>>>>>>>>>\e[0m"
-yum module enable redis:remi-6.2 -y
+echo -e "\e[35m<<<<<<<<<<  enable redis6.2  >>>>>>>>>\e[0m"
+yum module enable redis:remi-6.2 -y &>>${log}
+func_exit_status
 
-echo -e "\e[35m<<<<<<<<<<redis installation successfully>>>>>>>>>\e[0m"
-yum install redis -y
+echo -e "\e[35m<<<<<<<<<<  install redis  >>>>>>>>>\e[0m"
+yum install redis -y &>>${log}
+func_exit_status
 
 echo -e "\e[35m<<<<<<<<<<Update listen address from 127.0.0.1 to 0.0.0.0>>>>>>>>>\e[0m"
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
 
-echo -e "\e[35m<<<<<<<<<<redis started successfully>>>>>>>>>\e[0m"
-systemctl enable redis
-systemctl restart redis
+echo -e "\e[35m<<<<<<<<<<  start redis  >>>>>>>>>\e[0m"
+systemctl enable redis &>>${log}
+systemctl restart redis &>>${log}
+func_exit_status
